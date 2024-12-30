29c51e87ad37622ccfba838d15ab332eafefaa37
Timmi-tech <tomiwatomzy66@gmail.com>
2024-12-30 15:39:48 +0100

    generating html through javacscript

--- data/cart.js
+++ data/cart.js
@@ -1,16 +1,19 @@
-export let cart = JSON.parse(localStorage.getItem('cart')) || [];;
-// if (!cart) {
-//     cart = [{
-//         productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-//         quantity: 1,
-//     }, {
-//         productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-//         quantity: 1,
-//     }, {
-//         productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
-//         quantity: 1,
-//     }];
-// }
+export let cart = JSON.parse(localStorage.getItem('cart'));
+if (!cart) {
+    cart = [{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 1,
+        deliveryOptionsId: '1'
+    }, {
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1,
+        deliveryOptionsId: '2'
+    }, {
+        productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
+        quantity: 1,
+        deliveryOptionsId: '3'
+    }];
+}
 
 function saveToStorage() {
     localStorage.setItem('cart', JSON.stringify(cart))
@@ -33,7 +36,8 @@ export function addToCart(productId) {
     } else {
         cart.push({
             productId,
-            quantity: selectedquantity
+            quantity: selectedquantity,
+            deliveryOptionsId: '1'
         })
     }
     saveToStorage();
